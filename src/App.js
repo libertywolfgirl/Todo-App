9a542152a8cd31b1eb4231292258bff4fad4534c
Glitch (starter-react-template) <none>
2021-04-17 19:39:55 +0000

    🍁🕰 Checkpoint ./src/App.js:16231546/517 ./src/components/Form.js:16231546/569 ./public/index.html:16231546/342 ./src/components/TodoList.js:16231546/479

--- src/App.js
+++ src/App.js
@@ -1,12 +1,16 @@
 import React from "react";
 import "./App.css";
+// Importing Components
+import Form from './components/Form';
+import TodoList from 
 
 function App() {
   return (
     <div className="App">
       <header>
-      <h1>Ed's Todo List</h1>
-    </header>
+        <h1>Dani's Todo List</h1>
+      </header>
+      <Form />
     </div>
   );
 }
