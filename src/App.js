a68377c828557987e1bf4952dec8d53481c4b05a
Glitch (starter-react-template) <none>
2021-04-17 19:51:55 +0000

    🎣🦀 Checkpoint ./src/App.js:16231546/141 ./src/components/Form.js:16231546/216

--- src/App.js
+++ src/App.js
@@ -1,16 +1,18 @@
-import React from "react";
+import React, {useState} from "react";
 import "./App.css";
 // Importing Components
 import Form from './components/Form';
-import TodoList from 
+import TodoList from './components/TodoList'
 
 function App() {
+  const [inputText, setInputText] = useState('');
   return (
     <div className="App">
       <header>
         <h1>Dani's Todo List</h1>
       </header>
-      <Form />
+      <Form setInputText={setInputText}/>
+      <TodoList />
     </div>
   );
 }
