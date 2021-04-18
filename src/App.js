9c41a7f2e8f7c9c0663f9dea618802a909274fc6
Glitch (starter-react-template) <none>
2021-04-18 00:23:16 +0000

    👨‍❤️‍👨🐥 Checkpoint ./src/App.js:16231546/2711

--- src/App.js
+++ src/App.js
@@ -5,10 +5,25 @@ import Form from "./components/Form";
 import TodoList from "./components/TodoList";
 
 function App() {
+  // State stuff
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
   const [status, setStatus] = useState("all");
   const [filteredTodos, setFilteredTodos] = useState([]);
+  // Functions
+  const filterHandler = () => {
+    switch (status) {
+      case "completed":
+        setFilteredTodos(todos.filter(todo => todo.completed === true));
+        break;
+      case "uncompleted":
+        setFilteredTodos(todos.filter(todo => todo.completed === false));
+        break;
+      default:
+        setFilteredTodos(todos);
+        break;
+    }
+  };
   return (
     <div className="App">
       <header>
