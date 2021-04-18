8a4c53f5ec8b1c2b24e77333eec1a718e3e9828d
Glitch (starter-react-template) <none>
2021-04-18 00:11:15 +0000

    💓🏀 Checkpoint ./src/components/Todo.js:16231546/3332 ./src/App.js:16231546/1531 ./src/components/Form.js:16231546/2389

--- src/App.js
+++ src/App.js
@@ -7,6 +7,8 @@ import TodoList from "./components/TodoList";
 function App() {
   const [inputText, setInputText] = useState("");
   const [todos, setTodos] = useState([]);
+  const [status, setStatus] = useState("all");
+  const [filteredTodos, setFilteredTodos] = useState([]);
   return (
     <div className="App">
       <header>
@@ -17,6 +19,7 @@ function App() {
         todos={todos}
         setTodos={setTodos}
         setInputText={setInputText}
+        setStatus={setStatus}
       />
       <TodoList todos={todos} setTodos={setTodos} />
     </div>
--- src/components/Todo.js
+++ src/components/Todo.js
@@ -10,7 +10,8 @@ const Todo = ({ text, todo, todos, setTodos }) => {
       todos.map(item => {
         if (item.id === todo.id) {
           return {
-            ...item, completed: !item.completed
+            ...item,
+            completed: !item.completed
           };
         }
         return item;
@@ -19,8 +20,10 @@ const Todo = ({ text, todo, todos, setTodos }) => {
   };
   return (
     <div className="todo">
-      <li className="todo-item">{text}</li>
-      <button onClick={} className="complete-btn">
+      <li className={`todo-item ${todo.completed ? "completed" : ""}`}>
+        {text}
+      </li>
+      <button onClick={completeHandler} className="complete-btn">
         <i className="fas fa-check"></i>
       </button>
       <button onClick={deleteHandler} className="trash-btn">
